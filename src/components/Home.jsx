872beb7b023e9d1cd16e0b2f9d7d4b21e5afd5bd
Unccepted <devuiandrew@gmail.com>
2023-07-22 00:29:04 +0300

    fix homw and nab

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import header from "../imgs/header.jpg";
+import youtubeIcon from "../imgs/youtube-icon-svgrepo-com.png";
+import tiktokIcon from "../svg/tiktok-logo-logo-svgrepo-com.svg";
+import instagramIcon from "../imgs/instagram-1-svgrepo-com.png";
 
 export function Home() {
   return (
     <section>
       <div class="py-20 md:py-28">
         <div class="flex flex-wrap xl:items-center ">
-          <div className="container px-4 mx-auto flex">
-            <div class="w-full md:w-1/2 px-4 mb-16 md:mb-0">
+          <div className="container px-4 mx-auto flex  max-md:flex-col-reverse ">
+            <div class="w-full md:w-1/2 px-4 mb-16 md:mb-0 ">
               <h1
-                class="mb-6 text-2xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight"
+                class="mb-6 text-2xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight text-gray-800 "
                 data-config-id="auto-txt-14-1"
               >
                 Создаем продающий контент, для продвижения вашего бизнеса.
@@ -18,18 +21,20 @@ export function Home() {
                 class="mb-8 text-lg md:text-xl text-gray-500 font-medium"
                 data-config-id="auto-txt-15-1"
               >
-                We’re different. Flex is the only saas business platform that
-                lets you run your business on one platform, seamlessly across
-                all digital channels.
+                Мы специализируемся на создании продающего контента, обращаясь
+                непосредственно к целевой аудитории. С опытом работы с
+                компаниями, обслуживающими возрастную категорию 40+,
+                разрабатываем индивидуальные решения, которые приносят
+                результаты
               </p>
               <div class="flex flex-wrap">
-                <div class="w-full md:w-auto py-1 md:py-0 md:mr-4">
+                <div class="w-fu  ll md:w-auto py-1 md:py-0 md:mr-4">
                   <a
                     class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-green-50 font-medium text-center bg-green-500 hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-green-500 rounded-md shadow-sm"
                     href="#"
                     data-config-id="auto-txt-16-1"
                   >
-                    Get Started
+                    Начать работу
                   </a>
                 </div>
                 <div class="w-full md:w-auto py-1 md:py-0 md:mr-4">
@@ -38,27 +43,34 @@ export function Home() {
                     href="#"
                     data-config-id="auto-txt-16-1"
                   >
-                    Book a call
+                    Запланировать звонок
                   </a>
                 </div>
               </div>
             </div>
-            <div class="w-full md:w-1/2 px-4">
+            <div class="relative w-full md:w-1/2 px-4 ">
               <div class="relative mx-auto md:mr-0 max-w-max">
                 <img
-                  class="absolute z-10 -left-14 -top-12 w-28 md:w-auto"
-                  src="flex-ui-assets/elements/circle3-yellow.svg"
+                  class="absolute rounded-md p-1 z-10 -left-14 -top-12  sm:w-14 sm:-left-6 sm:-top-6 md:w-16 md:-left-8 md:-top-8 lg:w-24 "
+                  src={youtubeIcon}
                   alt=""
                   data-config-id="auto-img-5-1"
                 />
                 <img
-                  class="absolute z-10 -right-7 -bottom-8 w-28 md:w-auto"
-                  src="flex-ui-assets/elements/dots3-blue.svg"
+                  class="absolute rounded-md p-1 z-10 -right-7 -bottom-8  sm:w-14 md:w-16  lg:w-24 "
+                  src={instagramIcon}
                   alt=""
                   data-config-id="auto-img-6-1"
                 />
                 <img
-                  class="relative rounded-7xl"
+                  class="absolute rounded-md p-1 z-10 -left-12 bottom-4  sm:w-14 sm:-left-4 md:w-16  md:-left-10 lg:w-24 "
+                  src={tiktokIcon}
+                  alt=""
+                  data-config-id="auto-img-6-1"
+                />
+
+                <img
+                  class="relative rounded-7xl "
                   src={header}
                   alt=""
                   data-config-id="auto-img-3-1"
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ export function Navigation() {
       <div className="bg-transparent container px-4 mx-auto">
         <nav className="flex justify-between p-6 px-4">
           <div className="flex justify-between items-center w-full">
-            <div className="w-1/2 xl:w-1/3 flex flex-row items-center">
+            <div className="w-1/2 xl:w-1/5 flex flex-row items-center">
               <div className="mr-2">
                 <a className="block max-w-max" href="#">
                   <img src={logo_img} alt="logo x-boost " className="h-12" />
@@ -17,14 +17,14 @@ export function Navigation() {
                 <img src={logo_text} alt="logo x-boost text" className="h-8" />
               </div>
             </div>
-            <div className="w-1/2 xl:w-1/3">
-              <ul className="hidden xl:flex xl:justify-center">
+            <div className="w-full xl:w-4/5 justify-end">
+              <ul className="hidden xl:flex xl:justify-center items-center">
                 <li className="mr-12">
                   <a
                     className="text-gray-500 hover:text-gray-900 font-medium"
                     href="#"
                   >
-                    Header-1
+                    Наши Примеры
                   </a>
                 </li>{" "}
                 <li className="mr-12">
@@ -48,7 +48,7 @@ export function Navigation() {
                     className="text-gray-500 hover:text-gray-900 font-medium"
                     href="#"
                   >
-                    Header-3
+                    Как это работает
                   </a>
                 </li>
                 <li className="mr-12">
@@ -56,17 +56,17 @@ export function Navigation() {
                     className="text-gray-500 hover:text-gray-900 font-medium"
                     href="#"
                   >
-                    Header-3
+                    Цена
                   </a>
                 </li>
+                <li className="mr-12">
+                  <div className="hidden xl:flex items-center justify-end">
+                    <button className="text-white hover:text-white-fade font-medium bg-green-500 hover:bg-green-600 py-2 px-4 rounded">
+                      Начать
+                    </button>
+                  </div>
+                </li>
               </ul>
-            </div>
-            <div className="w-1/2 xl:w-1/3">
-              <div className="hidden xl:flex items-center justify-end">
-                <button className="text-white hover:text-white-fade font-medium bg-emerald-600 hover:bg-emerald-800 py-2 px-4 rounded">
-                  Contact Us
-                </button>
-              </div>
             </div>
           </div>
         </nav>
